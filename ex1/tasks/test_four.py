4b6cb309e621e48f563dfa4fd13e80f4f85670d7
jigsaw <misho_1998@mail.ru>
2021-09-23 01:42:37 +0300

    update

--- ex1/tasks/test_four.py
+++ ex1/tasks/test_four.py
@@ -1,6 +1,7 @@
 """Test the Task data type."""
 
 from collections import namedtuple
+import pytest
 
 Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
 Task.__new__.__defaults__ = (None, None, False, None)
@@ -15,7 +16,7 @@ def test_asdict():
                 'id': 21}
     assert t_dict == expected
 
-
+@pytest.mark.run_these_please
 def test_replace():
     """replace() should change passed in fields."""
     t_before = Task('finish book', 'brian', False)
